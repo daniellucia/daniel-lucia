0cd67af554d735134e7dd00447710359bade418a
Daniel Lucia <daniellucia84@gmail.com>
2023-10-14 20:27:30 +0200

    Cambio de bloques

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -243,3 +243,65 @@ if (!function_exists('daniel_lucia_woocommerce_header_cart')) {
 add_action('init', function () {
 	remove_action('woocommerce_before_main_content', 'woocommerce_breadcrumb', 20, 0);
 });
+
+/**
+ * Movemos blqoues
+ */
+remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10 );
+remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_upsell_display', 15 );
+remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20 );
+
+add_action( 'woocommerce_custom_after_content', 'woocommerce_output_product_data_tabs', 10 );
+add_action( 'woocommerce_custom_after_content', 'woocommerce_upsell_display', 15 );
+add_action( 'woocommerce_custom_after_content', 'woocommerce_output_related_products', 20 );
+
+
+/**
+ * Plantilla para WooCommerce
+ */
+if ( ! function_exists( 'woocommerce_content' ) ) {
+
+	function woocommerce_content() {
+
+		if ( is_singular( 'product' ) ) {
+
+			while ( have_posts() ) :
+				the_post();
+				wc_get_template_part( 'content', 'single-product' );
+			endwhile;
+
+		} else {
+			?>
+
+			<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
+
+				<h1 class="page-title"><?php woocommerce_page_title(); ?></h1>
+
+			<?php endif; ?>
+
+			<?php do_action( 'woocommerce_archive_description' ); ?>
+
+			<?php if ( woocommerce_product_loop() ) : ?>
+
+				<?php do_action( 'woocommerce_before_shop_loop' ); ?>
+
+				<?php woocommerce_product_loop_start(); ?>
+
+				<?php if ( wc_get_loop_prop( 'total' ) ) : ?>
+					<?php while ( have_posts() ) : ?>
+						<?php the_post(); ?>
+						<?php wc_get_template_part( 'content', 'product' ); ?>
+					<?php endwhile; ?>
+				<?php endif; ?>
+
+				<?php woocommerce_product_loop_end(); ?>
+
+				<?php do_action( 'woocommerce_after_shop_loop' ); ?>
+
+				<?php
+			else :
+				do_action( 'woocommerce_no_products_found' );
+			endif;
+		}
+	}
+}
--- woocommerce.php
+++ woocommerce.php
@@ -3,7 +3,10 @@
 <div class="container-columns">
 
 	<main id="primary" class="site-main">
+
 		<?php woocommerce_content(); ?>
+        <?php do_action('woocommerce_custom_after_content'); ?>
+
     </main>
 
     <?php get_sidebar(); ?>
