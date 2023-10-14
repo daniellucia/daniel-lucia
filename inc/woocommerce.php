f999177b856c46a11b6808bd3b9db93f50c459f7
Daniel Lucia <daniellucia84@gmail.com>
2023-10-14 23:56:21 +0200

    Estilos imagenes

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -332,3 +332,39 @@ function custom_change_price_free($price, $product)
  */
 
 remove_action('woocommerce_order_details_after_order_table', 'woocommerce_order_again_button');
+
+
+/**
+ * AJAX
+ */
+
+ function daniellucia_ajax_add_to_cart_handler() {
+	WC_Form_Handler::add_to_cart_action();
+	WC_AJAX::get_refreshed_fragments();
+}
+add_action( 'wc_ajax_ace_add_to_cart', 'daniellucia_ajax_add_to_cart_handler' );
+add_action( 'wc_ajax_nopriv_ace_add_to_cart', 'daniellucia_ajax_add_to_cart_handler' );
+
+// Remove WC Core add to cart handler to prevent double-add
+remove_action( 'wp_loaded', array( 'WC_Form_Handler', 'add_to_cart_action' ), 20 );
+
+function daniel_luciaajax_add_to_cart_add_fragments($fragments)
+{
+	$all_notices  = WC()->session->get('wc_notices', array());
+	$notice_types = apply_filters('woocommerce_notice_types', array('error', 'success', 'notice'));
+
+	ob_start();
+	foreach ($notice_types as $notice_type) {
+		if (wc_notice_count($notice_type) > 0) {
+			wc_get_template("notices/{$notice_type}.php", array(
+				'notices' => array_filter($all_notices[$notice_type]),
+			));
+		}
+	}
+	$fragments['notices_html'] = ob_get_clean();
+
+	wc_clear_notices();
+
+	return $fragments;
+}
+add_filter('woocommerce_add_to_cart_fragments', 'daniel_luciaajax_add_to_cart_add_fragments');
--- js/woocommerce.js
+++ js/woocommerce.js
@@ -1,7 +1,52 @@
 jQuery(function ($) {
 	$(document).ready(function () {
 		$(document).on('added_to_cart', function (event, fragments, cart_hash, $button) {
-			alert('El producto se ha añadido al carrito');
+			/* alert('El producto se ha añadido al carrito');*/ 
+		});
+
+		$('form.cart').on('submit', function (e) {
+			e.preventDefault();
+
+			var form = $(this);
+			form.block({ message: null, overlayCSS: { background: '#fff', opacity: 0.6 } });
+
+			var formData = new FormData(form[0]);
+			formData.append('add-to-cart', form.find('[name=add-to-cart]').val());
+
+			$.ajax({
+				url: wc_add_to_cart_params.wc_ajax_url.toString().replace('%%endpoint%%', 'ace_add_to_cart'),
+				data: formData,
+				type: 'POST',
+				processData: false,
+				contentType: false,
+				complete: function (response) {
+					response = response.responseJSON;
+
+					if (!response) {
+						return;
+					}
+
+					if (response.error && response.product_url) {
+						window.location = response.product_url;
+						return;
+					}
+
+					if (wc_add_to_cart_params.cart_redirect_after_add === 'yes') {
+						window.location = wc_add_to_cart_params.cart_url;
+						return;
+					}
+
+					var $thisbutton = form.find('.single_add_to_cart_button'); 
+
+					$(document.body).trigger('added_to_cart', [response.fragments, response.cart_hash, $thisbutton]);
+
+					$('.woocommerce-error, .woocommerce-message, .woocommerce-info').remove();
+
+					form.closest('.product').before(response.fragments.notices_html)
+
+					form.unblock();
+				}
+			});
 		});
 	});
 });
