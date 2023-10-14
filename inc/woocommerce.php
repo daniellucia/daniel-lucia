2d018ab23163ef21f80802972f8af8ab702cbf34
Daniel Lucia <daniellucia84@gmail.com>
2023-10-14 22:10:18 +0200

    Cambiamos etiqueta nombre de producto

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -319,4 +319,13 @@ add_filter('woocommerce_product_tabs', function ($tabs) {
 	return [];
 }, 98);
 
+/**
+ * Sobrescribimos para dar estilo
+ */
 
+ if (!function_exists('woocommerce_template_loop_product_title')) {
+	function woocommerce_template_loop_product_title()
+	{
+		echo '<h3 class="' . esc_attr(apply_filters('woocommerce_product_loop_title_classes', 'woocommerce-loop-product__title')) . '">' . get_the_title() . '</h3>';
+	}
+}
