7aff5e148c80c184916421419a9c546e3f68dd63
Daniel Lucia <daniellucia84@gmail.com>
2023-10-14 21:16:37 +0200

    No mostramos pie en checkout

--- footer.php
+++ footer.php
@@ -1,9 +1,12 @@
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'daniel-lucia' ), 'daniel-lucia', '<a href="http://www.daniellucia.es">Daniel Lucia</a>' ); ?>
-		</div>
-	</footer>
+	<?php if (!is_checkout()): ?>
+		<footer id="colophon" class="site-footer">
+			<div class="site-info">
+				<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'daniel-lucia' ), 'daniel-lucia', '<a href="http://www.daniellucia.es">Daniel Lucia</a>' ); ?>
+			</div>
+		</footer>
+	<?php endif; ?>
+	
 </div>
 
 <?php wp_footer(); ?>
