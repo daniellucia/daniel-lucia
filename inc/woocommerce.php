3dd7e4e7f65c3a369773749a5504feaa2986ac81
Daniel Lucia <daniellucia84@gmail.com>
2023-10-16 17:07:05 +0200

    Mostramos total del carrito

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -172,7 +172,7 @@ if (!function_exists('daniel_lucia_woocommerce_cart_link')) {
 				WC()->cart->get_cart_contents_count()
 			);
 			?>
-			<span class="amount"><?php echo wp_kses_data(WC()->cart->get_cart_subtotal()); ?></span> <!-- <span class="count"><?php echo esc_html($item_count_text); ?></span> -->
+			<span class="amount"><?php echo wp_kses_data(WC()->cart->get_cart_subtotal()); ?></span> <span class="count"><?php echo esc_html($item_count_text); ?></span>
 		</a>
 	<?php
 	}
