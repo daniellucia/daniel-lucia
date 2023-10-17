464d1bb818887d9abb88c031827c0f058562f872
Daniel Lucia <daniellucia84@gmail.com>
2023-10-17 17:44:30 +0200

    FIX

--- single.php
+++ single.php
@@ -12,8 +12,8 @@
 
 				the_post_navigation(
 					array(
-						'prev_text' => '<span class="nav-title button">%title</span>',
-						'next_text' => '<span class="nav-title button">%title</span>',
+						'prev_text' => '<span class="nav-title button button-link">%title</span>',
+						'next_text' => '<span class="nav-title button button-link">%title</span>',
 					)
 				);
 
