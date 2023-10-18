31c08eaa2ae5b74bf5d2b04cc3dbcdb3a24f59a4
Daniel Lúcia <dlucia@refineriaweb.com>
2023-10-18 11:44:38 +0200

    Estilo comentarios

--- comments.php
+++ comments.php
@@ -26,25 +26,25 @@ if ( post_password_required() ) {
 	// You can start editing here -- including this comment!
 	if ( have_comments() ) :
 		?>
-		<h2 class="comments-title">
+		<h5 class="comments-title">
 			<?php
 			$daniel_lucia_comment_count = get_comments_number();
 			if ( '1' === $daniel_lucia_comment_count ) {
 				printf(
 					/* translators: 1: title. */
-					esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'daniel-lucia' ),
+					esc_html__( 'One thought on %1$s;', 'daniel-lucia' ),
 					'<span>' . wp_kses_post( get_the_title() ) . '</span>'
 				);
 			} else {
 				printf( 
 					/* translators: 1: comment count number, 2: title. */
-					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $daniel_lucia_comment_count, 'comments title', 'daniel-lucia' ) ),
+					esc_html( _nx( '%1$s thought on %2$s;', '%1$s thoughts on %2$s', $daniel_lucia_comment_count, 'comments title', 'daniel-lucia' ) ),
 					number_format_i18n( $daniel_lucia_comment_count ), // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 					'<span>' . wp_kses_post( get_the_title() ) . '</span>'
 				);
 			}
 			?>
-		</h2><!-- .comments-title -->
+		</h5><!-- .comments-title -->
 
 		<?php the_comments_navigation(); ?>
 
