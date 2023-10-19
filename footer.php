80aa18236f6d33f3d131a1837d7e1a72efb457cc
Daniel Lucia <daniellucia84@gmail.com>
2023-10-19 08:31:02 +0200

    Estilo footer

--- footer.php
+++ footer.php
@@ -1,13 +1,13 @@
-
-	<?php if (!is_checkout() && !is_cart()): ?>
-		<footer id="colophon" class="site-footer">
-			<div class="site-info">
-				<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'daniel-lucia' ), 'daniel-lucia', '<a href="http://www.daniellucia.es">Daniel Lucia</a>' ); ?>
-			</div>
-		</footer>
-	<?php endif; ?>
 	
 </div>
+
+<?php if (!is_checkout() && !is_cart()): ?>
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'daniel-lucia' ), 'daniel-lucia', '<a href="http://www.daniellucia.es">Daniel Lucia</a>' ); ?>
+		</div>
+	</footer>
+<?php endif; ?>
 
 <?php wp_footer(); ?>
 
