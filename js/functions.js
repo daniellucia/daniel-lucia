b9c11acb6c477757c8a338f654ee13254c8c0e70
Daniel Lucia <daniellucia84@gmail.com>
2023-10-24 19:34:12 +0200

    fix

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
     const el = document.querySelector("div.site-header-container")
-    console.log(el)
     const observer = new IntersectionObserver(
         ([e]) => e.target.classList.toggle("is-pinned", e.intersectionRatio < 1),
         { threshold: [1] }
